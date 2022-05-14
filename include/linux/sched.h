9429932cce0f77d6f289a0209b5bd3f189954daf
Marco <1151883@isep.ipp.pt>
2022-05-15 00:26:07 +0100

    Task1 - 1st iteration

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1487,7 +1487,7 @@ struct task_struct {
 	 */
 	struct callback_head		l1d_flush_kill;
 #endif
-
+	int rtosp;
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
--- init/init_task.c
+++ init/init_task.c
@@ -214,6 +214,7 @@ struct task_struct init_task
 #ifdef CONFIG_SECCOMP_FILTER
 	.seccomp	= { .filter_count = ATOMIC_INIT(0) },
 #endif
+	.rtosp = 0,
 };
 EXPORT_SYMBOL(init_task);
 
